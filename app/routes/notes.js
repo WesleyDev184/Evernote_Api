83b5d41ce57ca93f71a4146d54c2d5e9eb575c72
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-09-05 21:08:20 -0300

    Finalizado
    Api finalizada

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -16,6 +16,18 @@ router.post('/', WithAuth, async(req, res) =>{
     }
 })
 
+router.get('/search', WithAuth, async(req, res) =>{
+    const { query } = req.query;
+    try {
+        let notes = await Note
+        .find({ author: req.user._id })
+        .find({ $text: { $search: query }});
+        res.json(notes);
+    } catch (error) {
+        res.json({error: error}).status(500);
+    }
+})
+
 router.get('/:id',WithAuth, async(req, res) => {
     try {
         const { id } = req.params;
@@ -56,6 +68,21 @@ router.put('/:id', WithAuth, async(req, res) =>{
         }
     } catch (error) {
         res.status(500).json({error: 'Problme to update a note'});
+    }
+})
+
+router.delete('/:id', WithAuth, async(req, res) =>{
+    const { id } = req.params;
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            await note.delete();
+            res.json({message: 'OK'}).status(204) 
+        } else {
+            res.status(403).json({error: 'Permision denied'});
+        }
+    } catch (error) {
+        res.status(500).json({error: 'Problme to delete a note'});
     }
 })
 
